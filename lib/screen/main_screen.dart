6c78d41f8423cbb86ace3b5b4687cb13d2e74b74
yangwenry <yangwenry@gmail.com>
2024-08-27 11:01:27 +0900

    메인페이지 개발

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -11,14 +11,20 @@ class MainScreen extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '점\n메\n추',
-              style: Theme.of(context).textTheme.displayLarge,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                '점\n메\n추',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
             ),
-            Container(child: const Text('-------------------------')),
-            Text(
-              '심\n뉴\n천',
-              style: Theme.of(context).textTheme.displayLarge,
+            Image.asset('assets/images/white_line.jpg', width: 100),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                '심\n뉴\n천',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
             ),
           ],
         ),
