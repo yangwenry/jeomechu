b271c44e7ea0e73ef0bd62f219aac61af3f31ab2
yangwenry <yangwenry@gmail.com>
2024-08-27 14:20:41 +0900

    메인페이지 수정

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -3,22 +3,40 @@ import 'package:flutter/material.dart';
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
+  final isLogin = false;
+
+  void init() {
+    if (isLogin) {
+      // 로그인 되어 있다면 위치정보제공 확인
+    } else {
+      // 로그인 되어 있지않다면 로그인 페이지로 이동
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    init();
+
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
       body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              '점\n메\n추',
-              style: Theme.of(context).textTheme.displayLarge,
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                '점\n메\n추',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
             ),
-            Container(child: const Text('-------------------------')),
-            Text(
-              '심\n뉴\n천',
-              style: Theme.of(context).textTheme.displayLarge,
+            Image.asset('assets/images/white_line.jpg', width: 100),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                '심\n뉴\n천',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
             ),
           ],
         ),
